afd593c0333cc91f9161506a3614406ecfef504d
bkklc <abkklc@gmail.com>
2023-11-12 20:48:47 +0300

    Yazılım Geliştirici Yetiştirme Kampı 4. Gün C#

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class ProductManager
+    {
+        // encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine("Ürün Eklendi: " + product.ProductName);
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine("Ürün Güncellendi: " + product.ProductName);
+        }
+
+
+         
+
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Channels;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 250;
+            product1.UnitsInStock = 5;
+
+            Product product2 = new Product
+            {
+                Id = 1,
+                CategoryId = 5,
+                UnitsInStock = 4,
+                ProductName = "Kalem",
+                UnitPrice = 35
+            };
+
+            // instance
+            // PascalCase  // camelCase
+            // case sensitive - küçük büyük harf duyarlılığı
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            productManager.Add(product2);
+            Console.WriteLine(product1.ProductName);
+
+
+        }
+    }
+}
